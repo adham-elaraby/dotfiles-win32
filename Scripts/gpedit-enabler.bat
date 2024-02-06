2d83c30eee11c020bc5ee7153598a98ae74f2e10
Adham Elaraby <63326537+adham-elaraby@users.noreply.github.com>
2024-02-06 15:10:39 +0100

    Added gpedit-enabler script

---
+++ Scripts/gpedit-enabler.bat
@@ -0,0 +1,9 @@
+@echo off 
+@echo "This batch file will try to enable Group Policy Editor (gpedit.msc) on Windows 10 Home."
+pushd "%~dp0" 
+
+dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~3*.mum >List.txt 
+dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~3*.mum >>List.txt 
+
+for /f %%i in ('findstr /i . List.txt 2^>nul') do dism /online /norestart /add-package:"%SystemRoot%\servicing\Packages\%%i" 
+pause
